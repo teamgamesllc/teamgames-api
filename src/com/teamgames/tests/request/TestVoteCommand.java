83e895ac5206b030ba412f524af5f7fdf1fa77e5
Nelson Sanchez <nelson@teamgames.io>
2024-03-23 08:59:08 -0400

    Tests

--- src/com/teamgames/tests/request/TestVoteCommand.java
+++ src/com/teamgames/tests/request/TestVoteCommand.java
@@ -3,7 +3,7 @@ package com.teamgames.tests.request;
 public class TestVoteCommand {
 	
 	public static void main(String mainArgs[]) {
-		String playerCommand = "reward 123 1";
+		String playerCommand = "reward 123 1"; 
 		String playerName = "REPLACE_WITH_PLAYER_NAME"; // Example: c.getUsername(), c.playerName(), c.getDisplayName()
 
 		/**
