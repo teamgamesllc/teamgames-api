8b99ac188cbaf15a3629ec514c1f04e5fbed9f68
Nelson Sanchez <nelson@teamgames.io>
2024-04-12 16:42:48 -0400

    Revert

---
+++ src/com/teamgames/vote/RewardService.java
@@ -0,0 +1,10 @@
+package com.teamgames.vote;
+
+import java.util.Map;
+
+public class RewardService {
+    public final Map<Integer, Integer> rewards;
+    public RewardService(Map<Integer, Integer> rewards) {
+        this.rewards = rewards;
+    }
+}
